d2f79fe427c250f15846c8f7b023d55721819739
Mia <miamorticia@Mias-MBP.hsd1.co.comcast.net>
2021-08-13 17:45:35 -0600

    user login

---
+++ components/Layout.js
@@ -0,0 +1,106 @@
+import React, { useContext } from 'react';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import Image from 'next/image';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
+  Badge,
+} from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
+import useStyles from '../utils/styles';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
+import tffa from '../public/images/tffa.png';
+
+export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode, cart } = state;
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+  const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>
+          {title ? `${title} - Tim Furlow Fine Art` : 'Tim Furlow Fine Art'}
+        </title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Image src={tffa} alt="Tim Furlow Fine Art" />
+                {/* <Typography className={classes.brand}>Tim Furlow Fine Art</Typography> */}
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>
+                  {cart.cartItems.length > 0 ? (
+                    <Badge
+                      color="secondary"
+                      badgeContent={cart.cartItems.length}
+                    >
+                      Cart
+                    </Badge>
+                  ) : (
+                    'Cart'
+                  )}
+                </Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Tim Furlow Fine Art.</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
+}
